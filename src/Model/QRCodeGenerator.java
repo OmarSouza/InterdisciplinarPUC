6b47d600024a9078c3d36cae121532346201b747
gcsvieira <74613475+gcsvieira@users.noreply.github.com>
2021-05-17 23:57:20 -0300

    Adição da livraria Zxing

---
+++ src/Model/QRCodeGenerator.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author guuil
+ */
+
+// Teste com a Livraria QRCode
+public class QRCodeGenerator {
+    
+    public void generateQRCode() throws WriterException, IOException{
+    String data = "www.google.com";
+    String path = "C:\\Users\\guuil\\OneDrive\\Documentos\\GitHub\\InterdisciplinarPUC\\testeQRCode.jpg";
+    
+    BitMatrix matrix = new MultiFormatWriter()
+            .encode(data, BarcodeFormat.QR_CODE, 500, 500);
+    
+    MatrixToImageWriter.writeToPath(matrix, "jpg", Paths.get(path));
+    }
+    
+}
